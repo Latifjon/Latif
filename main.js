23cea77f8783a4e0cab645031f21de9070e81404
Latifjon <110631919+Latifjon@users.noreply.github.com>
2025-04-01 07:49:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//const mydiv = document.createElement("myDIV").innerHTML = img src="/images/ovqat1" alt = "shashlik"
+//console.log(mydiv)
+
+
+let photo = document.createElement('img');
+photo.alt = 'ovqat';
+photo.src = 'shashlik.png'
+myDIV.appenchild(photo)
+
+//mydiv.appendChild(photo)
+//document.getElementById("myDIV").appendChild(photo)
+console.log(photo);
